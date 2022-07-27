f6aa23c9c5ad1173d403d657b71773068fc59f4f
Kai Guo <guokai8@gmail.com>
2022-07-26 21:48:49 -0500

    Update misc.R

--- R/misc.R
+++ R/misc.R
@@ -74,6 +74,45 @@ print.O2pls <- function (x, ...) {
     summary(x)
 }
 
+#' @title Summary of an plsda object
+#'
+#' @param object a plsda object
+#' @param ... For consistency
+#' @return Detail of plsda results
+#' @examples 
+#' X <- matrix(rnorm(500),10,50)
+#' Y <- rep(c("a","b"),each=5)
+#' fit <- plsda(X,Y,2)
+#' summary(fit)
+#' @author Kai Guo
+#' @export
+summary.plsda <- function(object, ...){
+    cat("\n######### Summary of the PLS-DA results #########\n")
+    cat("### Call plsda(X, Y, nc=",object$nc,") ###\n")
+    R2X <- object$xvar
+    R2Y <- object$R2Y
+    R2Xcum <- cumsum(R2X)
+    R2Ycum <- cumsum(R2Y)
+    res <- rbind(R2X,R2Xcum,R2Y,R2Ycum)
+    print(round(res,3))
+    cat("\n############################################\n")
+}
+
+#' @title Print the summary of plsda results.
+#' @param x An plsda object 
+#' @param ... For consistency
+#' @return NULL
+#' @examples 
+#' X <- matrix(rnorm(500),10,50)
+#' Y <- rep(c("a","b"),each=5)
+#' fit <- plsda(X,Y,2)
+#' print(fit)
+#' @author Kai Guo
+#' @export
+print.plsda <- function (x, ...) {
+    summary(x)
+}
+
 #' Extract the VIP values from the O2PLS-DA object
 #' @param x the o2plsda object or plsda object
 #' @return a data frame 
